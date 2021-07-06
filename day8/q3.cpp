a52ac4b3fbf3b5554397c88357b0ebbde380a560
kartik78946 <67784095+kartik78946@users.noreply.github.com>
2021-07-06 17:11:36 +0530

    Create q3.cpp

---
+++ day8/q3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int i,j,count=0;
+        for(i=0;i<grid.size();i++)
+        {
+            for(j=0;j<grid[i].size();j++)
+            {
+                if(grid[i][j]<0)
+                {
+                    count = count+(grid[i].size())-j;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
